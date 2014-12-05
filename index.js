56aaff7fd19e4842bf72300efb7bf10e65642778
jonschlinkert <dev@sellside.com>
2014-12-05 08:48:16 -0500

    faster implementation

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// faster slice
 var slice = require('array-slice');
 
 /**
@@ -18,14 +17,9 @@ module.exports = function extend(o) {
 
   var args = slice(arguments, 1);
   var len = args.length;
-  var i = 0;
 
-  if (len === 0) {
-    return o;
-  }
-
-  while (len--) {
-    var obj = args[i++];
+  for (var i = 0; i < len; i++) {
+    var obj = args[i];
 
     for (var key in obj) {
       if (obj.hasOwnProperty(key)) {
@@ -33,5 +27,6 @@ module.exports = function extend(o) {
       }
     }
   }
+
   return o;
 };
