820a7f42f9952c1f4bac5b44fa66633f0127c71f
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-25 03:26:25 -0500

    adds tests

--- test.js
+++ test.js
@@ -1,19 +1,30 @@
 /*!
  * extend-shallow <https://github.com/jonschlinkert/extend-shallow>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
 
-var should = require('should');
+var path = require('path');
+var argv = require('minimist')(process.argv.slice(2));
 var extend = require('./');
+require('should');
+
+if (argv && argv.lib) {
+  extend = require(path.resolve('benchmark/code', argv.lib));
+}
 
 describe('extend', function () {
   it('should extend the first object with the properties of the other objects.', function () {
     extend({a: 'b'}, {c: 'd'}).should.eql({a: 'b', c: 'd'});
     extend({a: 'b', c: 'd'}, {c: 'e'}).should.eql({a: 'b', c: 'e'});
+  });
+
+  it('should skip over non-objects.', function () {
+    extend({a: 'b'}, 'foo', {c: 'd'}).should.eql({a: 'b', c: 'd'});
+    extend({a: 'b', c: 'd'}, 'bar', {c: 'e'}).should.eql({a: 'b', c: 'e'});
   });
 
   it('should return an empty object when args are undefined.', function () {
