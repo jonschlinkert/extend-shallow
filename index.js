4cbab73ded171c7f1dd2e9869c262496be81a39b
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-25 03:26:06 -0500

    refactor:
     - externalize assign function, faster in larger objects
     - don't slice args for v8 optimization

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 'use strict';
 
-var slice = require('array-slice');
+var isObject = require('is-plain-object');
 
 /**
- * Extend the target `obj` with the properties of other objects.
+ * Expose `extend`
+ */
+
+module.exports = extend;
+
+/**
+ * Extend `o` with properties of other `objects`.
  *
- * @param  {Object}  `obj` The target object. Pass an empty object to shallow clone.
- * @param  {Objects}
+ * @param  {Object} `o` The target object. Pass an empty object to shallow clone.
+ * @param  {Object} `objects`
  * @return {Object}
  */
 
-module.exports = function extend(o) {
-  if (o == null) {
-    return {};
-  }
-
-  var args = slice(arguments, 1);
-  var len = args.length;
-
+function extend(o, objects) {
+  if (!o || !objects) { return o || {}; }
+  var len = arguments.length - 1;
   for (var i = 0; i < len; i++) {
-    var obj = args[i];
-
-    for (var key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        o[key] = obj[key];
-      }
+    var obj = arguments[i + 1];
+    if (isObject(obj)) {
+      assign(o, obj);
     }
   }
-
   return o;
-};
+}
+
+function assign(o, obj) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      o[key] = obj[key];
+    }
+  }
+  return o;
+}
