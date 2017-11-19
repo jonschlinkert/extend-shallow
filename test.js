d8e1965237d7b0d3f72a281782df971db9956c88
jonschlinkert <jon.schlinkert@sellside.com>
2017-11-19 09:46:57 -0500

    run update

--- test.js
+++ test.js
@@ -7,7 +7,7 @@
 
 'use strict';
 
-/* deps: mocha */
+require('mocha');
 var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
 var extend = require('./');
@@ -17,51 +17,51 @@ if (argv && argv.lib) {
   extend = require(path.resolve('benchmark/code', argv.lib));
 }
 
-describe('extend', function () {
-  it('should extend the first object with the properties of the other objects.', function () {
+describe('extend', function() {
+  it('should extend the first object with the properties of the other objects.', function() {
     extend({a: 'b'}, {c: 'd'}).should.eql({a: 'b', c: 'd'});
     extend({a: 'b', c: 'd'}, {c: 'e'}).should.eql({a: 'b', c: 'e'});
   });
 
-  it('should skip over non-plain objects.', function () {
+  it('should skip over non-plain objects.', function() {
     extend({a: 'b'}, 'foo', {c: 'd'}).should.eql({a: 'b', c: 'd'});
     extend({a: 'b'}, null, {c: 'd'}).should.eql({a: 'b', c: 'd'});
     extend({a: 'b'}, new Date(), {c: 'd'}).should.eql({a: 'b', c: 'd'});
     extend({a: 'b', c: 'd'}, 'bar', {c: 'e'}).should.eql({a: 'b', c: 'e'});
   });
 
-  it('should extend a regex.', function () {
+  it('should extend a regex.', function() {
     var fixture = /foo/;
     extend(fixture, {a: 'b'}, new Date(), {c: 'd'});
     fixture.a.should.equal('b');
     fixture.c.should.equal('d');
   });
 
-  it('should extend a function.', function () {
+  it('should extend a function.', function() {
     var fixture = function() {};
     extend(fixture, {a: 'b'}, new Date(), {c: 'd'});
     fixture.a.should.equal('b');
     fixture.c.should.equal('d');
   });
 
-  it('should extend an array.', function () {
+  it('should extend an array.', function() {
     var arr = [];
     extend(arr, {a: 'b'}, new Date(), {c: 'd'});
     arr.a.should.equal('b');
     arr.c.should.equal('d');
   });
 
-  it('should return an empty object when args are undefined.', function () {
+  it('should return an empty object when args are undefined.', function() {
     extend(null).should.eql({});
     extend(undefined).should.eql({});
   });
 
-  describe('.extend():', function () {
-    it('should extend object a with object b:', function () {
+  describe('.extend():', function() {
+    it('should extend object a with object b:', function() {
       extend({a: {b: 'b'}}, {b: {c: 'c'}}).should.eql({a: {b: 'b'}, b: {c: 'c'}});
     });
 
-    it('should return an empty object when args are undefined:', function () {
+    it('should return an empty object when args are undefined:', function() {
       extend().should.eql({});
     });
   });
